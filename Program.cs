b56b8037b567ada18b14ea3258a25f8db6405166
SPlkv <73382208+SPlkv@users.noreply.github.com>
2021-01-11 19:04:26 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -16,45 +16,54 @@ namespace LAB3
     {
         static void Main(string[] args)
         {
+            dynamic excelApp = Activator.CreateInstance(Type.GetTypeFromProgID("Excel.Application"));
+                                //можно и так:
+            //dynamic excelApp = new Application();
+            excelApp.Workbooks.Open(@"C:\Users\User\Desktop\Lab3.1.xlsm");
+            excelApp.Visible = true;
+            dynamic workSheet = excelApp.ActiveSheet;
+
             //Импорт из Excel в C#
-            Application ObjExcel = new Application();
-            var pathToFile = @"C:\Users\User\Desktop\Lab3.1.xlsm";
-            Workbook ObjWorkBook = ObjExcel.Workbooks.Open(pathToFile, 0, false, 5, "", "", false, XlPlatform.xlWindows, "", true, false, 0, true, false, false);
-            Worksheet ObjWorkSheet;
-            ObjWorkSheet = (Worksheet)ObjWorkBook.Sheets[1];
-            ObjExcel.Visible = true;
+            //Application ObjExcel = new Application();
+            //var pathToFile = @"C:\Users\User\Desktop\Lab3.1.xlsm";
+            //Workbook ObjWorkBook = ObjExcel.Workbooks.Open(pathToFile, 0, false, 5, "", "", false, XlPlatform.xlWindows, "", true, false, 0, true, false, false);
+            //Worksheet ObjWorkSheet;
+            //ObjWorkSheet = (Worksheet)ObjWorkBook.Sheets[1];
+            //ObjExcel.Visible = true;
 
             //Список доступных функций
-            Range xlRange = ObjWorkSheet.Range["A1","A4"];
+            Range xlRange = excelApp.Range["A1", "A4"];
             foreach (Range c in xlRange.Rows.Cells)
             {
                 Console.WriteLine("Функция: " + c.Value);
             }
 
-            //Запись выбранной функции в ячейку F2
+            ////Запись выбранной функции в ячейку F2
             Console.WriteLine("Какую функцию посчитать?");
             int answer = Convert.ToInt32(Console.ReadLine());
-            ObjWorkSheet.Cells[2, "F"] = answer;
+            workSheet.Cells[2, "F"] = answer;
 
-            //Запись X
+            ////Запись X
             Console.WriteLine("Выберите значение X");
             int x = Convert.ToInt32(Console.ReadLine());
-            ObjWorkSheet.Cells[3, "F"] = x;
+            workSheet.Cells[3, "F"] = x;
 
-            //Таблица значений
-            ObjWorkSheet.Cells[9, "A"] = "Таблица значений";
-            ObjWorkSheet.Cells[10, "A"] = "X";
-            ObjWorkSheet.Cells[10, "B"] = "Y";
+            ////Таблица значений
+            workSheet.Cells[9, "A"] = "Таблица значений";
+            workSheet.Cells[10, "A"] = "X";
+            workSheet.Cells[10, "B"] = "Y";
             for (var i = 0; i <= x; i++)
             {
-                
-                ObjWorkSheet.Cells[3, "F"] = i;
-                ObjWorkSheet.Cells[11+i, "A"] = i;
-                ObjWorkSheet.Cells[11+i, "B"] = ObjWorkSheet.Cells[6, "F"];               
+
+                workSheet.Cells[3, "F"] = i;
+                workSheet.Cells[11 + i, "A"] = i;
+                workSheet.Cells[11 + i, "B"] = workSheet.Cells[6, "F"];
             }
             
-            //Построение диаграммы
-            ChartObjects xlCharts = (ChartObjects)ObjWorkSheet.ChartObjects(Type.Missing);
+            
+
+            ////Построение диаграммы
+            ChartObjects xlCharts = (ChartObjects)workSheet.ChartObjects(Type.Missing);
             ChartObject myChart = xlCharts.Add(10, 80, 300, 250);
             Chart chartPage = myChart.Chart;
             myChart.Select();
@@ -65,9 +74,10 @@ namespace LAB3
 
 
             Series series1 = seriesCollection.NewSeries();
-            series1.XValues = ObjWorkSheet.get_Range("A11", "A100"); 
-            series1.Values = ObjWorkSheet.get_Range("B11", "B100");
+            series1.XValues = excelApp.get_Range("A11", "A100");
+            series1.Values = excelApp.get_Range("B11", "B100");
 
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
             Console.Read();
         }
     }
